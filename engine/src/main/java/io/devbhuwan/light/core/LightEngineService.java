a2c0e6cd515c0c976f5d54f586722c2aca288372
Bhuwan Prasad Upadhyay <comp.er.bhuwan@gmail.com>
2017-08-16 18:12:44 +0400

    light-platform setup

---
+++ engine/src/main/java/io/devbhuwan/light/core/LightEngineService.java
@@ -0,0 +1,10 @@
+package io.devbhuwan.light.core;
+
+/**
+ * <p></p>
+ *
+ * @author Bhuwan Prasad Upadhyay
+ */
+public interface LightEngineService {
+
+}
---
+++ engine/src/main/java/io/devbhuwan/light/core/LightPlatform.java
@@ -0,0 +1,11 @@
+package io.devbhuwan.light.core;
+
+/**
+ * <p></p>
+ *
+ * @author Bhuwan Prasad Upadhyay
+ */
+public class LightPlatform {
+
+
+}
